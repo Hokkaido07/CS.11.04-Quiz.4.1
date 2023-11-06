c7a0c86671419984839981695eb13f1408c424ae
Hokkaido <alexisc2026@student.cis.edu.hk>
2023-11-06 12:30:54 +0800

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
+import static java.lang.Character.toUpperCase;
+
 public class Main {
 
     /** 1. Write a method called numberOfVowels that returns the number of vowels in a string.
      Consider a, e, i, o, u as vowels. **/
-    
+
 
     /** 2. Write a method called notDivisibleBy235 that finds the number of natural numbers not exceeding N
      and not divisible by any of the numbers [2, 3, 5].
@@ -16,14 +20,61 @@ public class Main {
      Answer: 1 (only one number doesn't divide integer by any of 2, 3, 5) **/
 
 
-    /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
+    /**
+     * 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
      * The first word within the output should be capitalized only if the original word was capitalized
      * (known as Upper Camel Case, also often referred to as Pascal case).
      * The next words should always be capitalized.
-     Examples
-     "the-stealth-warrior" gets converted to "theStealthWarrior"
-     "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
-     "The_Stealth-Warrior" gets converted to "TheStealthWarrior" **/
+     * Examples
+     * "the-stealth-warrior" gets converted to "theStealthWarrior"
+     * "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+     * "The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+     **/
 
+    public static int numberOfVowels(String str) {
+        int count = 0;
+        int length = str.length();
+
+        for (int i = 0; i < length; i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'o' || str.charAt(i) == 'i' || str.charAt(i) == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int notDivisibleBy235(int n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static String toCamelCase(String input) {
+        StringBuilder result = new StringBuilder();
+        boolean capitalizeNext = false;
+
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+
+            if (currentChar == '-' || currentChar == '_') {
+                capitalizeNext = true;
+            } else {
+                if (capitalizeNext) {
+                    result.append(Character.toUpperCase(currentChar));
+                    capitalizeNext = false;
+                } else if (Character.isUpperCase(currentChar)) {
+                    result.append(currentChar);
+                } else {
+                    result.append(Character.toLowerCase(currentChar));
+                }
+            }
+        }
+
+        return result.toString();
+    }
 
 }
